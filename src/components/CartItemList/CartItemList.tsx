730732610cc67e5d4e8f39473f92a7f843881f39
lamara <lamaralanchava@gmail.com>
2023-07-24 16:33:53 +0300

    cart page markup done

--- src/components/CartItemList/CartItemList.tsx
+++ src/components/CartItemList/CartItemList.tsx
@@ -1,20 +1,29 @@
 import Grid from "@mui/material/Grid";
 
-import styles from './styles.module.css'
 import {CartItem} from "../CartItem/CartItem";
+import styles from './styles.module.css'
 
 export const CartItemList = () => {
-  let arr = [1, 2, 3, 4]
+  let arr = [1, 2]
 
   return (
     <div className={styles.mainContainer}>
-      <Grid container className={styles.itemsContainer}>
-        {arr.map((elem) => (
-          <Grid xs={12} item>
-            <CartItem/>
-          </Grid>
-        ))}
-      </Grid>
+      <div className={styles.itemsContainer}>
+        <Grid xs={12} container justifyContent="center" className={styles.itemsTitleContainer}>
+          <p className={styles.itemsTitle}>Корзина</p>
+        </Grid>
+        <Grid container spacing={3.75}>
+          {arr.map((elem) => (
+            <Grid xs={12} item>
+              <CartItem/>
+            </Grid>
+          ))}
+        </Grid>
+        <Grid xs={12} container justifyContent="space-between" className={styles.itemsFinalCostContainer}>
+          <p className={styles.itemsFinalCostTitle}>Итого</p>
+          <p className={styles.itemsFinalCost}>Цена&nbsp;₽</p>
+        </Grid>
+      </div>
     </div>
   )
 }
